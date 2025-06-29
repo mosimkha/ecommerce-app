7c118035dd0f231a5cb8d8b566783d52d1b18ac2
mosimkha <pathan.mohsin73@gmail.com>
2025-06-29 15:42:24 +0530

    build error

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,6 +1,6 @@
 // src/context/CartContext.tsx
-import { createContext, useContext, useState, ReactNode } from 'react';
-
+import { createContext, useContext, useState } from 'react';
+import type { ReactNode } from 'react';
 export type CartItem = {
   id: number;
   title: string;
